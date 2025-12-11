2f6dde68515ea08832fc1d9b601aacb653bdb0ef
Enzo Wanderley Tomazoni <enzo.wanderley@academico.ifpb.edu.br>
2025-12-11 13:15:17 -0300

    conclusão da atividade

--- app.py
+++ app.py
@@ -9,23 +9,47 @@ app.secret_key = 'sua_chave_secreta_aqui'
 # Configuração dos produtos (simulando um banco de dados)
 app.config['PRODUTOS'] = {
     1: {
+        'tipo': 'higiene',
         'id': 1, 
-        'nome': 'Produto 1', 
+        'nome': 'Desodorante Rexona',
         'preco': 10.00, 
-        'imagem_url': 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Product_sample_icon_picture.png/640px-Product_sample_icon_picture.png'
+        'imagem_url': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdtOKiF_oxD9HS9K4VIeb1V7Hjl1-PEGm1TQ&s'
     },
     2: {
+        'tipo': 'nuclear',
         'id': 2, 
-        'nome': 'Produto 2', 
-        'preco': 20.00, 
-        'imagem_url': 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Product_sample_icon_picture.png/640px-Product_sample_icon_picture.png'
+        'nome': 'Urânio enriquecido 1KG', 
+        'preco': 2000000.00, 
+        'imagem_url': 'https://c.files.bbci.co.uk/79CF/production/_107838113_gettyimages-844443408-1.jpg'
     },
-    3: {
+    3: { 
+        'tipo': 'nuclear',
         'id': 3, 
-        'nome': 'Produto 3', 
-        'preco': 30.00, 
-        'imagem_url': 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Product_sample_icon_picture.png/640px-Product_sample_icon_picture.png'
+        'nome': 'Césio-137', 
+        'preco': 5000000.00, 
+        'imagem_url': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJuPjeNIrysKx8XAvL70dms1Q1AL_kOe4tIA&s'
     },
+    4: {
+        'tipo': 'nuclear',
+        'id': 4,
+        'nome': 'Bomba de Hidrogênio',
+        'preco': 10000000.00,
+        'imagem_url': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRdwnnn1Xnkwt-JutPrHOrSWVogEfsDDq2yQ&s'
+    },
+    5: {
+        'tipo': 'nuclear',
+        'id': 5,
+        'nome': 'átomo de amendoim',
+        'preco': 0.01,
+        'imagem_url': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3XrTuPC9wFVOOUMRlhD7ATcbmJ9VAVw5Suw&s'
+    },
+    6: {
+        'tipo': 'nuclear',
+        'id': 6,
+        'nome': 'FranBaby',
+        'preco': 999999999.99,
+        'imagem_url': 'https://media-gru2-2.cdn.whatsapp.net/v/t61.24694-24/567323385_1156398146595020_1602469055019689819_n.jpg?ccb=11-4&oh=01_Q5Aa3QFDfm5ShTmUlL4xwqG3_E9PLokzRzJPONa9Y3qT1Qu1bg&oe=6946E8B0&_nc_sid=5e03e0&_nc_cat=106'
+    }
 }
 
 # Rota principal - Lista de produtos
@@ -36,14 +60,15 @@ def index():
 
 # Rota para adicionar produto ao carrinho
 @app.route('/adicionar/<int:produto_id>', methods=['POST'])
-def adicionar_ao_carrinho(produto_id):
+def adicionar_ao_carrinho(produto_id, qtd=1):
     produtos = app.config['PRODUTOS']
     
     # Recuperar carrinho da sessão (ou criar lista vazia)
     carrinho = session.get('carrinho', [])
     
     # Adicionar produto ao carrinho
-    carrinho.append(produtos[produto_id])
+    for _ in range(int(request.form.get('quantidade', 1))):
+        carrinho.append(produtos[produto_id])
     
     # Salvar carrinho na sessão
     session['carrinho'] = carrinho
@@ -80,6 +105,28 @@ def remover_do_carrinho(produto_id):
     
     return redirect(url_for('carrinho'))
 
+@app.route('/remover_tudo', methods=['POST'])
+def remover_tudo():
+    carrinho = session.get('carrinho', [])
+    
+    carrinho.clear()   
+    
+    session['carrinho'] = carrinho 
+    flash('Carrinho Limpo!')
+    
+    return redirect(url_for('carrinho'))
+
+@app.route('/filtrar/<tipo>')
+def filtrar(tipo):
+    produtos = app.config['PRODUTOS']
+    produtos_filtrados = {id: p for id, p in produtos.items() if p['tipo'] == tipo}
+    return render_template('index.html', produtos=produtos_filtrados)
+
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
+
 # Executar aplicação
 if __name__ == '__main__':
     app.run(debug=True)
+
